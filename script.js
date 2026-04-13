a7a808ad600d9fd67955b69c70ba7573363c33e7
Rosiane Sales <dossantosrosiane7@gmail.com>
2026-04-13 15:53:26 -0300

    JAVA
    OK

--- script.js
+++ script.js
@@ -31,15 +31,44 @@ formulario.addEventListener('submit', validarFormulario);
             }
         }
         btnTema.addEventListener('click', alternarTema);
+         
+        const meusProjetos = [
+            {
+                título: "Projeto 01 - Cine Top",
+                descriçao: "Site que mostra a lista dos filmes mais assistidos do momento com uma breve descrição de cada um.",
+                link: "https://github.com/rosianesalesweb"
+            },
+            {
+                título: "Projeto 02 - Portfólio",
+                descriçao: "Meu portfólio de apresentação feito com HTML, CSS e JavaScript.",
+                link: "https://github.com/rosianesalesweb"
+            },
+            {
+                título:"Projeto 03 - Boutique de Mimos (Moda e Beleza)",
+                descriçao: " Uma vitrine virtual ficticia de perfumes, maquiagens e acessórios femininos, para praticar o visual de uma loja online.",
+                link:"https://github.com/rosianesalesweb"    
+            }
+            ];
 
-        
+            
+            function renderizarProjetos() {
+                const container = document.querySelector('.projetos-container');
 
+                container.innerHTML = "";
 
-                 
-              
- 
+                meusProjetos.forEach(projeto => {
+                    const cardHTML = `
+                    <article class="card-projeto">
+                    <h3>${projeto.título}</h3>
+                    <p>${projeto.descriçao}</p>
+                    <a href="${projeto.link}" target="_blank">Ver no gitHub</a>
+                    </article>
+                    `;
+                    container.innerHTML += cardHTML;
+                    });
+            }
 
-
+            renderizarProjetos ();
 
 
 
